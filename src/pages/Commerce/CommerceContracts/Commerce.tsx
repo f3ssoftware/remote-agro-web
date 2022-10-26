5dcca22a8c8a8469b20e899bacda7ccf1a0b5c92
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2022-10-26 17:48:02 -0300

    btn design

--- src/pages/Commerce/CommerceContracts/Commerce.tsx
+++ src/pages/Commerce/CommerceContracts/Commerce.tsx
@@ -1,12 +1,31 @@
+import "./Commerce.scss";
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+
 export function Commerce() {
     return (
         <div className="container">
             <div className="frist-column">
                 <div className="frist-card">
-                    <span>Cultura</span>
+                    <Dropdown className="frist-card-dropdown">
+                        <span className="frist-card-text">Cultura</span>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Milho
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Cultura1</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Cultura2</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Cultura3</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
                 <div className="second-card">
-                    <span>Contratos</span>
+                    <span className="second-card-text">Contratos</span>
+                    <div>
+                        <Button variant="success" className="second-card-button">+</Button>{' '}
+                    </div>
+
                 </div>
             </div>
         </div>
