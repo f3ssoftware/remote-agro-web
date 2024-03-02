c8f509005112a62cc8e77e151212e5e34649434e
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2024-03-02 15:41:28 -0300

    defensive prescription application id

--- src/stores/plot.store.tsx
+++ src/stores/plot.store.tsx
@@ -6,171 +6,178 @@ import { Application } from "../models/Application";
 import { getMessages } from './messaging.store'
 import { ApplicationTable } from "../models/ApplicationTable";
 
-const initialApplications:any[] = [];
+const initialApplications: any[] = [];
 const initialAppliers: Applier[] = []
-const initialApplicationData: Application[]=[]
+const initialApplicationData: Application[] = []
 const initialApplicationTables: ApplicationTable = {}
-const plotStore = createSlice({
-    name: 'plot',
-    initialState: {
-        applications: initialApplications,
-        appliers: initialAppliers,
-        applicationData: initialApplicationData,
-        applicationTables: initialApplicationTables
+const initialPrescription: any = {};
 
+const plotStore = createSlice({
+  name: 'plot',
+  initialState: {
+    applications: initialApplications,
+    appliers: initialAppliers,
+    applicationData: initialApplicationData,
+    applicationTables: initialApplicationTables,
+    prescription: initialPrescription,
+  },
+  reducers: {
+    setApplications(state, action) {
+      state.applications = action.payload;
     },
-    reducers: {
-        setApplications(state, action) {
-            state.applications = action.payload;
-        },
-        setAppliers(state, action){
-            state.appliers = action.payload;
-        },
-        setApplicationData(state,action){
-            state.applicationData = action.payload
-        },
-        setApplicationTables(state,action){
-          state.applicationData = action.payload
-      }
-            
+    setAppliers(state, action) {
+      state.appliers = action.payload;
+    },
+    setApplicationData(state, action) {
+      state.applicationData = action.payload
+    },
+    setApplicationTables(state, action) {
+      state.applicationData = action.payload
+    },
+    setPrescription(state, action) {
+      state.prescription = action.payload;
     }
+
+  }
 });
 
 
-export const { setApplications, setAppliers, setApplicationData, setApplicationTables } = plotStore.actions;
+export const { setApplications, setAppliers, setApplicationData, setApplicationTables, setPrescription } = plotStore.actions;
 export default plotStore.reducer;
 
 
 export function asyncFetchApplications(params: any) {
-    return async function (dispatch: AppDispatch) {
-        try {
-            const results = await axios.get(
-                "https://remoteapi.murilobotelho.com.br/applications",
-                {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-                    },
-                    params
-                },
-            );
-            dispatch(setApplications(results.data.list))
-        } catch (err) {
-            console.error(err);
-        }
+  return async function (dispatch: AppDispatch) {
+    try {
+      const results = await axios.get(
+        "https://remoteapi.murilobotelho.com.br/applications",
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+          params
+        },
+      );
+      dispatch(setApplications(results.data.list))
+    } catch (err) {
+      console.error(err);
+    }
 
-    };
+  };
 }
 
 
 export function asyncFetchAppliers(params: any) {
-    return async function (dispatch: AppDispatch) {
-        try {
-            const results = await axios.get(
-                "https://remoteapi.murilobotelho.com.br/appliers",
-                {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-                    },
-                    params
-                },
-            );
-            dispatch(setAppliers(results.data))
-        } catch (err) {
-            console.error(err);
-        }
+  return async function (dispatch: AppDispatch) {
+    try {
+      const results = await axios.get(
+        "https://remoteapi.murilobotelho.com.br/appliers",
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+          params
+        },
+      );
+      dispatch(setAppliers(results.data))
+    } catch (err) {
+      console.error(err);
+    }
 
-    };
+  };
 }
 export function asyncFetchApplicationData() {
-    return async function (dispatch: AppDispatch) {
-        try {
-            const results = await axios.get(
-                "https://remoteapi.murilobotelho.com.br/applications",
-                {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-                    },
-                },
-            );
-            dispatch(setApplicationData(results.data))
-        } catch (err) {
-            console.error(err);
-        }
+  return async function (dispatch: AppDispatch) {
+    try {
+      const results = await axios.get(
+        "https://remoteapi.murilobotelho.com.br/applications",
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        },
+      );
+      dispatch(setApplicationData(results.data))
+    } catch (err) {
+      console.error(err);
+    }
 
-    };
+  };
 }
 
 export function asyncPrescription(prescription: any) {
-    return async function (dispatch: AppDispatch) {
-      try {
-        const result = await axios.post(
-          `https://remoteapi.murilobotelho.com.br/applications`,
-          prescription,
-          {
-            headers: {
-              Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-            },
+  return async function (dispatch: AppDispatch) {
+    try {
+      const result = await axios.post(
+        `https://remoteapi.murilobotelho.com.br/applications`,
+        prescription,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`,
           },
-        )
-        dispatch(
-          getMessages({
-            message: 'Receituário  salvo com sucesso',
-            type: 'success',
-          }),
-        )
-      } catch (err: any) {
-        dispatch(
-          getMessages({
-            message: err.response.data.message,
-            type: 'error',
-          }),
-        )
-      }
-    }
-  }
-  export function asyncPrescriptionTable(prescription: any) {
-    return async function (dispatch: AppDispatch) {
-      try {
-        const result = await axios.post(
-          `https://remoteapi.murilobotelho.com.br/application-tables`,
-          prescription,
-          {
-            headers: {
-              Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-            },
-          },
-        )
-        // dispatch(
-        //   getMessages({
-        //     message: 'Receituário  salvo com sucesso',
-        //     type: 'success',
-        //   }),
-        // )
-      } catch (err: any) {
-        dispatch(
-          getMessages({
-            message: err.response.data.message,
-            type: 'error',
-          }),
-        )
-      }
-    }
-  }
-  export function asyncFetchApplicationTables() {
-    return async function (dispatch: AppDispatch) {
-        try {
-            const results = await axios.get(
-                "https://remoteapi.murilobotelho.com.br/applications-tables",
-                {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-                    },
-                },
-            );
-            dispatch(setApplicationTables(results.data))
-        } catch (err) {
-            console.error(err);
-        }
+        },
+      )
 
-    };
+      dispatch(setPrescription(result.data));
+      dispatch(
+        getMessages({
+          message: 'Receituário  salvo com sucesso',
+          type: 'success',
+        }),
+      )
+    } catch (err: any) {
+      dispatch(
+        getMessages({
+          message: err.response.data.message,
+          type: 'error',
+        }),
+      )
+    }
+  }
+}
+export function asyncPrescriptionTable(prescription: any) {
+  return async function (dispatch: AppDispatch) {
+    try {
+      const result = await axios.post(
+        `https://remoteapi.murilobotelho.com.br/application-tables`,
+        prescription,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+          },
+        },
+      )
+      // dispatch(
+      //   getMessages({
+      //     message: 'Receituário  salvo com sucesso',
+      //     type: 'success',
+      //   }),
+      // )
+    } catch (err: any) {
+      dispatch(
+        getMessages({
+          message: err.response.data.message,
+          type: 'error',
+        }),
+      )
+    }
+  }
+}
+export function asyncFetchApplicationTables() {
+  return async function (dispatch: AppDispatch) {
+    try {
+      const results = await axios.get(
+        "https://remoteapi.murilobotelho.com.br/applications-tables",
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        },
+      );
+      dispatch(setApplicationTables(results.data))
+    } catch (err) {
+      console.error(err);
+    }
+
+  };
 }
