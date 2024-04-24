c0826727043162fbb95d0d5f10cf75fbfdd2f5e7
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-04-23 21:22:41 -0300

    some prime and detail last changes

--- src/components/Navbar/SeasonsSelection.tsx
+++ src/components/Navbar/SeasonsSelection.tsx
@@ -4,94 +4,121 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../..'
 import { selectSeason } from '../../stores/seasons.store'
 import { SeasonsSelectionModal } from './Modals/SeasonsSelectionModal'
-import { asyncFetchInputWeighingData, asyncFetchOutputWeighingData, asyncFetchWeighingData } from '../../stores/commerce.store'
+import {
+  asyncFetchInputWeighingData,
+  asyncFetchOutputWeighingData,
+  asyncFetchWeighingData,
+} from '../../stores/commerce.store'
 import { Dialog } from 'primereact/dialog'
-import { dialogContentSyle, dialogHeaderStyle } from '../../utils/modal-style.util'
+import {
+  dialogContentSyle,
+  dialogHeaderStyle,
+} from '../../utils/modal-style.util'
+import { Dropdown } from 'primereact/dropdown'
 
 export function SeasonSelection({ show, handleClose }: any) {
   const { seasons } = useSelector((state: RootState) => state)
-  const [selectedSeason, setSelectedSeason]: any = useState({});
+  const [selectedSeason, setSelectedSeason]: any = useState({})
   const dispatch = useDispatch<any>()
   const confirm = () => {
-    console.log(selectedSeason);
-    dispatch(selectSeason(selectedSeason));
+    console.log(selectedSeason)
+    dispatch(selectSeason(selectedSeason))
     handleClose()
   }
-  const [showSeasonsSelectionModal, setShowSeasonsSelectionModal] = useState(false)
+  const [showSeasonsSelectionModal, setShowSeasonsSelectionModal] =
+    useState(false)
 
   useEffect(() => {
-    dispatch(asyncFetchInputWeighingData(seasons?.selectedSeason?.id));
-    dispatch(asyncFetchOutputWeighingData(seasons?.selectedSeason?.id));
+    dispatch(asyncFetchInputWeighingData(seasons?.selectedSeason?.id))
+    dispatch(asyncFetchOutputWeighingData(seasons?.selectedSeason?.id))
 
     if (selectedSeason?.id !== seasons.selectedSeason.id) {
       setSelectedSeason(seasons.selectedSeason)
     }
-
   }, [seasons])
   return (
     <Container>
       <Dialog
-      header="Temporada"
-      visible={show}
-      style={{ width: '50vw' }}
-      className="custom-dialog"
-      onHide={handleClose}
-      headerStyle={dialogHeaderStyle}
-      contentStyle={dialogContentSyle}
-    >
-      
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="">
-                <Form.Label style={{ color: '#fff' }}>
-                  Selecione a temporada
-                </Form.Label>
-                <Form.Select
-                  value={JSON.stringify(selectedSeason)}
-                  aria-label=""
-                  onChange={(e) => {
-                    // dispatch(selectSeason(e.target.value))
-                    // handleClose()
-                    console.log(e.target.value);
-                    setSelectedSeason(JSON.parse(e.target.value));
-                  }}
-                >
-                  {seasons.seasons.map((season, index) => {
-                    return (
-                      <option
-                        key={index}
-                        value={JSON.stringify(season)}
-                      >{`${season.type} - ${season.year}`}</option>
-                    )
-                  })}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button
-                style={{ border: 'none', backgroundColor: '#243C74' }}
-                variant="success"
-                onClick={() => setShowSeasonsSelectionModal(true)}
-              >
-                Adicionar
-              </Button>
-            </Col>
-            <Col>
-              <Button style={{ color: '#000' }}
-                variant="success"
-                onClick={() => {
-                  confirm()
-                  handleClose()
+        header="Temporada"
+        visible={show}
+        style={{ width: '50vw' }}
+        className="custom-dialog"
+        onHide={handleClose}
+        headerStyle={dialogHeaderStyle}
+        contentStyle={dialogContentSyle}
+      >
+        <Row>
+          <Col>
+            <span className="p-float-label">
+              <Dropdown
+                value={selectedSeason}
+                options={seasons.seasons.map((season) => ({
+                  label: `${season.type} - ${season.year}`,
+                  value: season,
+                }))}
+                onChange={(e) => {
+                  // dispatch(selectSeason(e.value))
+                  // handleClose()
+                  setSelectedSeason(e.value)
+                }}
+                placeholder="Selecione a temporada"
+              />
+              <label htmlFor="outcomeYear">Ano agrícola</label>
+            </span>
+            {/* <Form.Group className="mb-3" controlId="">
+              <Form.Label style={{ color: '#fff' }}>
+                Selecione a temporada
+              </Form.Label>
+              <Form.Select
+                value={JSON.stringify(selectedSeason)}
+                aria-label=""
+                onChange={(e) => {
+                  // dispatch(selectSeason(e.target.value))
+                  // handleClose()
+                  console.log(e.target.value)
+                  setSelectedSeason(JSON.parse(e.target.value))
                 }}
               >
-                Confirmar
-              </Button>
-            </Col>
-          </Row>
-    </Dialog>
-      <SeasonsSelectionModal show={showSeasonsSelectionModal} handleClose={() => setShowSeasonsSelectionModal(false)}></SeasonsSelectionModal>
+                {seasons.seasons.map((season, index) => {
+                  return (
+                    <option
+                      key={index}
+                      value={JSON.stringify(season)}
+                    >{`${season.type} - ${season.year}`}</option>
+                  )
+                })}
+              </Form.Select>
+            </Form.Group> */}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              style={{ border: 'none', backgroundColor: '#243C74' }}
+              variant="success"
+              onClick={() => setShowSeasonsSelectionModal(true)}
+            >
+              Adicionar
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              style={{ color: '#000' }}
+              variant="success"
+              onClick={() => {
+                confirm()
+                handleClose()
+              }}
+            >
+              Confirmar
+            </Button>
+          </Col>
+        </Row>
+      </Dialog>
+      <SeasonsSelectionModal
+        show={showSeasonsSelectionModal}
+        handleClose={() => setShowSeasonsSelectionModal(false)}
+      ></SeasonsSelectionModal>
     </Container>
   )
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,62 +1,86 @@
-import { Button } from "react-bootstrap";
-import logo from "../../assets/images/logoLogin.png";
-import Form from 'react-bootstrap/Form';
-import "./Login.scss"
-import { useEffect, useState } from "react";
-import { asyncFetchUser } from "../../stores/user.store";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { RootState } from "../..";
-import { InputText } from "primereact/inputtext";
-import { Password } from "primereact/password";
+import { Button } from 'react-bootstrap'
+import logo from '../../assets/images/logoLogin.png'
+import Form from 'react-bootstrap/Form'
+import './Login.scss'
+import { useEffect, useState } from 'react'
+import { asyncFetchUser } from '../../stores/user.store'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { RootState } from '../..'
+import { InputText } from 'primereact/inputtext'
+import { Password } from 'primereact/password'
 
 export function Login() {
-    let navigate = useNavigate();
-    const dispatch = useDispatch<any>();
-    const { loading } = useSelector((state: RootState) => state);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  let navigate = useNavigate()
+  const dispatch = useDispatch<any>()
+  const { loading } = useSelector((state: RootState) => state)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-    const login = async () => {
-        // console.log(`Usuário: ${username} / Senha: ${password}`);
-        await dispatch(asyncFetchUser(username, password));
-        navigate("/");
-    }
+  const login = async () => {
+    // console.log(`Usuário: ${username} / Senha: ${password}`);
+    await dispatch(asyncFetchUser(username, password))
+    navigate('/')
+  }
 
-    return (
-        <div className="background-img">
-            <div className="login-square">
-                <img className="login-logo" src={logo} alt="" />
-                <Form onKeyUp={e => {
-                    if (e.key === 'Enter') {
-                        login();
-                    }
-                }}>
-                    <Form.Group className="mb-2 user-box" controlId="formBasicEmail">
-                        <span className="p-float-label">
-                            <InputText style={{ width: '100%' }} value={username} onChange={(e) => setUsername(e.target.value)} disabled={loading.requests.filter(r => r === 'sessions').length > 0} />
-                            <label htmlFor="username">Usuário</label>
-                        </span>
-                        {/* <Form.Label>Usuário</Form.Label>
+  return (
+    <div className="background-img">
+      <div className="login-square">
+        <img className="login-logo" src={logo} alt="" />
+        <Form
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              login()
+            }
+          }}
+        >
+          <Form.Group className="mb-2 user-box" controlId="formBasicEmail">
+            <span className="p-float-label">
+              <InputText
+                style={{ width: '100%' }}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                disabled={
+                  loading.requests.filter((r) => r === 'sessions').length > 0
+                }
+              />
+              <label htmlFor="username">Usuário</label>
+            </span>
+            {/* <Form.Label>Usuário</Form.Label>
                         <Form.Control type="text" onChange={e => setUsername(e.target.value)} disabled={loading.requests.filter(r => r === 'sessions').length > 0} /> */}
-                    </Form.Group>
+          </Form.Group>
 
-                    <Form.Group className="mb-2 password-box" controlId="formBasicPassword" style={{ marginTop: '7%' }}>
-                        <span className="p-float-label">
-                            <Password feedback={false} style={{ width: '100%' }} inputStyle={{ width: '100%' }} value={password} onChange={(e) => setPassword(e.target.value)} toggleMask disabled={loading.requests.filter(r => r === 'sessions').length > 0} />
-                            {/* <InputText value={password} onChange={(e) => setPassword(e.target.value)} disabled={loading.requests.filter(r => r === 'sessions').length > 0} /> */}
-                            <label htmlFor="username">Senha</label>
-                        </span>
-                        {/* <Form.Label>Senha</Form.Label>
+          <Form.Group
+            className="mb-2 password-box"
+            controlId="formBasicPassword"
+            style={{ marginTop: '7%' }}
+          >
+            <span className="p-float-label">
+              <Password
+                feedback={false}
+                style={{ width: '100%' }}
+                inputStyle={{ width: '100%' }}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                toggleMask
+                disabled={
+                  loading.requests.filter((r) => r === 'sessions').length > 0
+                }
+              />
+              {/* <InputText value={password} onChange={(e) => setPassword(e.target.value)} disabled={loading.requests.filter(r => r === 'sessions').length > 0} /> */}
+              <label htmlFor="username">Senha</label>
+            </span>
+            {/* <Form.Label>Senha</Form.Label>
                         <Form.Control type="password" onChange={e => setPassword(e.target.value)} /> */}
-                    </Form.Group>
-                </Form>
-                <div className="square-text">
-                    <a className="forgot-text">Esqueceu a senha?</a>
-                </div>
-                <Button className="btn-login" onClick={() => login()}>Entrar</Button>
-            </div>
+          </Form.Group>
+        </Form>
+        <div className="square-text">
+          <a className="forgot-text">Esqueceu a senha?</a>
         </div>
-
-    )
+        <Button className="btn-login" onClick={() => login()}>
+          Entrar
+        </Button>
+      </div>
+    </div>
+  )
 }
